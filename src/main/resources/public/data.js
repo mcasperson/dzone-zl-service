18e58434705e22f3112eb51579daf11920375377
Matthew Casperson <matthewcasperson@gmail.com>
2016-02-02 09:48:19 +1000

    Getting to work on Heroku

--- src/main/resources/public/data.js
+++ src/main/resources/public/data.js
@@ -56,6 +56,7 @@ function getAllImages() {
         var random = Math.floor(Math.random() * randomImageRange + randomImageStartIndex);
         if (jQuery("#imageId" + random).length == 0) {
             var listItem = jQuery("<li class='imageListItem'> \
+                                <label> \
                                 <input type='radio' class='image' name='radgroup' value='" + random + "' id='imageId" + random + "'> \
                                 <img src='https://dz2cdn1.dzone.com/thumbnail?fid=" + random + "&w=" + imageWidth + "'/> \
                                 </label> \
