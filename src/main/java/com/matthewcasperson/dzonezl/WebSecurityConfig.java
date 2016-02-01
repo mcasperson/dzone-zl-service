6838fd577f78bc317f069e9ffccdb7fd14f1fa1d
Matthew Casperson <matthewcasperson@gmail.com>
2016-02-01 14:14:44 +1000

    Getting to work on Heroku

--- src/main/java/com/matthewcasperson/dzonezl/WebSecurityConfig.java
+++ src/main/java/com/matthewcasperson/dzonezl/WebSecurityConfig.java
@@ -17,7 +17,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .anyRequest().authenticated()
                 .and()
                 .formLogin()
-                .loginPage("/login")
+                .loginPage("/login.html")
                 .permitAll()
                 .and()
                 .logout()
