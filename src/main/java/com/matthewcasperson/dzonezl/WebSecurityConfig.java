1e8442dbe52fdd34d3271639ebb64aa10873b0c8
Matthew Casperson <matthewcasperson@gmail.com>
2016-02-01 14:22:38 +1000

    Getting to work on Heroku

--- src/main/java/com/matthewcasperson/dzonezl/WebSecurityConfig.java
+++ src/main/java/com/matthewcasperson/dzonezl/WebSecurityConfig.java
@@ -13,6 +13,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
+                .csrf().disable()
                 .authorizeRequests()
                 .anyRequest().authenticated()
                 .and()
