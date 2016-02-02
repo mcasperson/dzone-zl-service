481bc12a74781711a25b39313f3ebb5ff803f5a7
Matthew Casperson <matthewcasperson@gmail.com>
2016-02-02 13:18:36 +1000

    Getting to work on Heroku

--- src/main/resources/public/domainimport.js
+++ src/main/resources/public/domainimport.js
@@ -17,28 +17,51 @@ submit.click(function() {
 
 function processText(contents) {
     var lines = contents.split("\n");
-    _.each(lines, function(line) {
-        var match = /^\s*<outline type="rss" text="(.*?)".*?xmlUrl="(.*?)".*?$/.exec(line)
-        if (match) {
-            var title = match[1];
-            var url = match[2];
 
-            var domainUri = URI(url);
+    async.eachSeries(
+        lines,
+        function(line, lineCallback) {
+            var match = /^\s*<outline type="rss" text="(.*?)".*?xmlUrl="(.*?)".*?$/.exec(line)
+            if (match) {
+                var title = match[1];
+                var url = match[2];
 
-            processDomain(domainUri, function(domainId) {
-                var titleSplit = title.split(/[^A-Za-z0-9@.]/);
+                var domainUri = URI(url);
 
-                _.each(titleSplit, function(titleElement) {
-                    if (titleElement.trim().length !== 0) {
-                        jQuery.get('https://dzone.com/services/widget/article-postV2/searchAuthors?q=' + titleElement, function(authors) {
-                            if (authors.result.data.length === 1) {
-                                var authorId = authors.result.data[0].id;
-                                addAuthors(authors.result.data, domainId);
+                processDomain(domainUri, function(domainId) {
+                    var titleSplit = title.split(/[^A-Za-z0-9@.]/);
+
+                    async.eachSeries(
+                        titleSplit,
+                        function(titleElement, authorCallback) {
+                            if (titleElement.trim().length !== 0) {
+                                jQuery.get('https://dzone.com/services/widget/article-postV2/searchAuthors?q=' + titleElement, function(authors) {
+                                    if (authors.result.data.length === 1) {
+                                        var authorId = authors.result.data[0].id;
+                                        addAuthors(authors.result.data, domainId);
+                                    }
+
+                                    authorCallback();
+                                });
+                            } else {
+                                async.setImmediate(function () {
+                                    authorCallback();
+                                });
                             }
-                        });
-                    }
+                        },
+                        function(err){
+                           console.log("Finished processing authors for " + domainUri.hostname());
+                        }
+                    );
                 });
-            });
+            } else {
+                async.setImmediate(function () {
+                    lineCallback();
+                });
+            }
+        },
+        function(err){
+            console.log("Finished processing hostname " + domainUri.hostname());
         }
-    });
+    );
 }
