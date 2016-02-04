486bd7a8632aaf12387a2d3755ff7a52f8630b1a
Matthew Casperson <matthewcasperson@gmail.com>
2016-02-04 10:41:40 +1000

    Uploading to Heroku

--- src/main/java/com/matthewcasperson/dzonezl/Application.java
+++ src/main/java/com/matthewcasperson/dzonezl/Application.java
@@ -1,12 +1,14 @@
 package com.matthewcasperson.dzonezl;
 
+import org.apache.catalina.filters.ExpiresFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.embedded.FilterRegistrationBean;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
-import java.util.Arrays;
-
+import javax.servlet.Filter;
 
 @SpringBootApplication
 @EnableTransactionManagement
@@ -22,5 +24,25 @@ public class Application {
         for (String beanName : beanNames) {
             System.out.println(beanName);
         }*/
+    }
+
+    @Bean
+    public FilterRegistrationBean tomcatExpiresFilter() {
+        final FilterRegistrationBean registration = new FilterRegistrationBean();
+        registration.setFilter(expiresFilter());
+        registration.addUrlPatterns("/*");
+        registration.addInitParameter("ExpiresByType image", "access plus 1 days");
+        registration.addInitParameter("ExpiresByType text/css", "access plus 1 days");
+        registration.addInitParameter("ExpiresByType text/css", "access plus 1 days");
+        registration.addInitParameter("ExpiresByType text/css", "access plus 1 days");
+        registration.addInitParameter("ExpiresByType application/javascript", "access plus 1 days");
+        registration.addInitParameter("ExpiresDefault", "access plus 1 days");
+        registration.setName("expiresFilter");
+        return registration;
+    }
+
+    @Bean(name = "expiresFilter")
+    public Filter expiresFilter() {
+        return new ExpiresFilter();
     }
 }
