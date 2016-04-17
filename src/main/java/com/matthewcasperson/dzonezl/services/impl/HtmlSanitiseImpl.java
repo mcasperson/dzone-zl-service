8cafab3392ed45a7ff76f9f2627cb7a77fd8f629
Matthew Casperson <matthewcasperson@gmail.com>
2016-04-17 12:48:53 +1000

    Uploading to Heroku

--- src/main/java/com/matthewcasperson/dzonezl/services/impl/HtmlSanitiseImpl.java
+++ src/main/java/com/matthewcasperson/dzonezl/services/impl/HtmlSanitiseImpl.java
@@ -4,6 +4,8 @@ import com.matthewcasperson.dzonezl.services.HtmlSanitiser;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
+import org.jsoup.nodes.Node;
+import org.jsoup.nodes.TextNode;
 import org.jsoup.select.Elements;
 import org.springframework.stereotype.Component;
 
@@ -19,11 +21,13 @@ public class HtmlSanitiseImpl implements HtmlSanitiser {
     public String sanitiseHtml(@NotNull final String input) {
         final Document doc = Jsoup.parse(input);
         fixUpImages(doc);
+        stripParentDivs(doc);
         return doc.toString();
     }
 
     /**
-     * Sometimes the imported content contains images with multiple srcs, which fails
+     * Sometimes the imported content contains images with multiple srcs, often from a
+     * srcset attribute. here we attempt to get the biggest image from that set.
      * @param doc
      */
     private void fixUpImages(final Document doc) {
@@ -42,6 +46,27 @@ public class HtmlSanitiseImpl implements HtmlSanitiser {
                         break;
                     }
                 }
+            }
+        }
+    }
+
+    /**
+     * Remove all divs that contain no text that are wrapping up the document
+     * @param doc
+     */
+    private void stripParentDivs(final Document doc) {
+        while(true) {
+            final Element div = doc.child(0);
+            if (div != null && div.tagName().equals("div")) {
+                for (final Node child : div.childNodes()) {
+                    if (child instanceof TextNode) {
+                        return;
+                    }
+                }
+
+                div.unwrap();
+            } else {
+                return;
             }
         }
     }
